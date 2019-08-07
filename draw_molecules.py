6f7d2a213e9cbb9e260e8d139174f7f595d6118a
jsmith13 <theintangiblefatman@gmail.com>
2019-08-07 13:01:15 -0700

    Converted drawings to black/white.

--- draw_molecules.py
+++ draw_molecules.py
@@ -1,9 +1,11 @@
-### Draw line representations of the compounds, store in 100 x 100 png files.
+### Draw line representations of the compounds, store in 300 x 300 png files.
 
 # load libraries
 from rdkit import Chem
 from rdkit.Chem import Draw
 import pandas as pd
+import PIL
+import csv
 
 # load the list of compounds
 compounds = pd.read_csv("compounds.csv")
@@ -11,14 +13,35 @@ compounds = pd.read_csv("compounds.csv")
 # generate rdkit molecules from the SMILES strings
 compounds["mol"] = compounds["SMILES"].apply(Chem.MolFromSmiles)
 
-# remove the structures that rdkit cannot understand
-compounds = compounds[compounds["mol"].isnull() == False]
+# the molecule drawer occasionally outputs an error while still producing an image
+# the try/except clause stops this from terminating the script
+# we will save the compounds that throw errors and write them out for later use
+errors = []
 
-# draw each molecule, write to file
-# the molecule drawer occasionally outputs an error while still producing an image, the try/except clause stops this from terminating the script
+# draw each molecule, save to file
 for i in range(len(compounds["mol"])):
     try:
-        Draw.MolToFile(compounds["mol"][i], fileName = "molecule_drawings/%s.png" %compounds["SubstanceID"][i], size = (300, 300))
+        # draw the molecule
+        molecule = Draw.MolToImage(compounds["mol"][i], size = (300, 300), kekulize = True)
+        
+        # a filter for taking pixel intensities to 0/1
+        def filter(x):
+            if x == 255:
+                return(255)
+            else:
+                return(0)
+
+        # convert the images into black and white
+        molecule = molecule.convert("L").point(filter, mode = "1")
+        
+        # save the image to a png file
+        molecule.save("molecule_drawings/%s.png" %compounds["SubstanceID"][i])
+        
     except:
-        print(i)
+        errors.append(compounds["SubstanceID"][i])
     
+# write out the substance IDs of the errors
+with open("errors.csv", "w") as csvfile:
+    writer = csv.writer(csvfile, delimiter = ",")
+    writer.writerow(errors)
+
